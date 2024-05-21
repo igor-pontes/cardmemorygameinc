562376024d6770ebaacd919b2d8e30e892053aac
Igor Andrade <igoppop@gmail.com>
2024-05-21 12:43:03 +0100

    work

--- main.c
+++ main.c
@@ -1,15 +1,21 @@
 #include <assert.h>
 #include <stdlib.h>
+#include <time.h>
 #include <stdio.h>
 #include <raylib.h>
+#include <stdint.h>
 
 #define DARKGRAY2 CLITERAL(Color){ 0, 0, 0, 1 }
 #define DARKYELLOW CLITERAL(Color) { 253, 249, 0, 230 }
 #define ROWS 4
 #define COLUMNS 4
+#define PAIR_SIZE ROWS + COLUMNS
 #define TEXTCOLOR WHITE
 #define ROUNDNESS 0.03
 #define SEGMENTS 1
+
+const Color colors[PAIR_SIZE] = { ORANGE, PINK, RED, GREEN, BLUE, MAROON, PURPLE, YELLOW };
+const char letters[PAIR_SIZE] = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H' };
 
 typedef enum { 
   GAMEOVER,
@@ -23,30 +29,91 @@ typedef struct GameManager {
 } GameManager;
 
 typedef struct Card {
-  char letter;
-  Color color;
   Rectangle shape;
   bool opened;
+  int pair_index;
 } Card;
 
-Card *createCards(int rows, int columns, int height, int width, float padding, Vector2 position) {
-  Card *cards = (Card*) malloc(rows * columns * sizeof(Rectangle));
-  for (int i = 0; i < rows; i++) {
-    for (int j = 0; j < columns; j++)
-      cards[i * columns + j] = (Card) { .letter = 'A', .shape.x = position.x + j * width + j * padding , .shape.y = position.y, .shape.width = width, .shape.height = height, .opened = false, .color = ORANGE };
+typedef struct CardPair {
+  Color color;
+  Card *card1;
+  Card *card2;
+  char letter[2];
+} CardPair;
+
+CardPair **createPairs() {
+  // Same reasoning as "createCards"
+  CardPair **pairs = (CardPair **) malloc(PAIR_SIZE * sizeof(CardPair*));
+  CardPair *temp = (CardPair *) malloc(PAIR_SIZE * sizeof(CardPair));
+  for (int i = 0; i < PAIR_SIZE; i++) {
+    temp[i].letter[1] = '\0';
+    pairs[i] = &temp[i];
+  }
+  return pairs;
+}
+
+void swapCards(Card **cards, int a, int b) {
+  cards[a] = (Card *) ((intptr_t) cards[a] ^ (intptr_t) cards[b]);
+  cards[b] = (Card *) ((intptr_t) cards[b] ^ (intptr_t) cards[a]);
+  cards[a] = (Card *) ((intptr_t) cards[a] ^ (intptr_t) cards[b]);
+}
+
+void swapPairs(CardPair **pairs, int a, int b) {
+  pairs[a] = (CardPair *) ((intptr_t) pairs[a] ^ (intptr_t) pairs[b]);
+  pairs[b] = (CardPair *) ((intptr_t) pairs[b] ^ (intptr_t) pairs[a]);
+  pairs[a] = (CardPair *) ((intptr_t) pairs[a] ^ (intptr_t) pairs[b]);
+}
+
+void resetPairs(Card **cards, CardPair** pairs) {
+  int last = PAIR_SIZE - 1;
+  for (int i = 0; i < PAIR_SIZE; i++) {
+    int current = rand() % (last+1);
+    if (current != last) swapPairs(pairs, current, last);
+    pairs[last]->color = colors[i];
+    last -= 1;
+  }
+
+  for (int i = 0; i < PAIR_SIZE; i++)
+   pairs[i]->letter[0] = letters[i];
+
+  last = COLUMNS * ROWS - 1;
+  for (int i = 0; i < PAIR_SIZE; i++) {
+    int current = rand() % (last+1);
+    if (last != current) swapCards(cards, current, last);
+    cards[last]->pair_index = i;
+    pairs[i]->card1 = &*cards[last];
+    last -= 1;
+    current = rand() % (last+1);
+    if (last != current) swapCards(cards, current, last);
+    cards[last]->pair_index = i;
+    pairs[i]->card2 = &*cards[last];
+    last -= 1;
+  }
+}
+
+Card **createCards(int height, int width, float padding, Vector2 position) {
+  // This way we anly need to allocate once, increasing performance.
+  // To free this block of memory, check lowest point in the cards array.
+  Card **cards = (Card **) malloc(ROWS * COLUMNS * sizeof(Card *));
+  Card *temp = (Card *) malloc(ROWS * COLUMNS * sizeof(Card));
+  for (int i = 0; i < ROWS; i++) {
+    for (int j = 0; j < COLUMNS; j++) {
+      int index = i * COLUMNS + j;
+      temp[index].shape = (Rectangle) { .x = position.x + j * width + j * padding , .y = position.y, .width = width, .height = height };
+      temp[index].opened = false; 
+      cards[index] = &temp[index];
+    }
     position.y += height + padding;
-  }  
+  }
   return cards;
 };
 
-void resetCards(Card* cards) { }
-
-void updateCards(Card* cards) {
+void updateCards(Card **cards, CardPair **pairs) {
   for (int i = 0; i < ROWS * COLUMNS; i++) {
-    if (cards[i].opened) {
-      DrawText("A", cards[i].shape.x + cards[i].shape.width/2 - 12, cards[i].shape.y + cards[i].shape.height/2 - 12, 50, cards[i].color);
+    if (cards[i]->opened) {
+      DrawText(pairs[cards[i]->pair_index]->letter, cards[i]->shape.x + cards[i]->shape.width/2 - 12, cards[i]->shape.y + cards[i]->shape.height/2 - 12, 50, pairs[cards[i]->pair_index]->color);
     } else {
-      DrawText("?", cards[i].shape.x + cards[i].shape.width/2 - 12, cards[i].shape.y + cards[i].shape.height/2 - 12, 50, DARKGRAY);
+      DrawText("?", cards[i]->shape.x + cards[i]->shape.width/2 - 12, cards[i]->shape.y + cards[i]->shape.height/2 - 12, 50, DARKGRAY);
     }
   }
 }
@@ -69,26 +136,26 @@ void drawCard(Card card, Color a, Color b) {
     DrawRectangleRounded(card.shape, ROUNDNESS, SEGMENTS, b);
 }
 
-void cardOnHover(Card* cards) {
+void cardOnHover(Card** cards, CardPair **pairs) {
   for (int i = 0; i < ROWS * COLUMNS; i++) {
     Vector2 m = GetMousePosition();
-    if (checkHit(m, cards[i].shape)) {
+    if (checkHit(m, cards[i]->shape)) {
       if (IsMouseButtonDown(MOUSE_LEFT_BUTTON)) {
-        DrawRectangleRounded(cards[i].shape, ROUNDNESS, SEGMENTS, DARKGRAY2);
+        DrawRectangleRounded(cards[i]->shape, ROUNDNESS, SEGMENTS, DARKGRAY2);
       } else {
-        drawCard(cards[i], LIGHTGRAY, modifiedColor(cards[i].color, 1./4));
+        drawCard(*cards[i], LIGHTGRAY, modifiedColor(pairs[cards[i]->pair_index]->color, 1./4));
       }
     } else {
-      drawCard(cards[i], WHITE, modifiedColor(cards[i].color, 1./2));
+      drawCard(*cards[i], WHITE, modifiedColor(pairs[cards[i]->pair_index]->color, 1./2));
     };
   }
 }
 
-void cardOnRelease(Card* cards) {
+void cardOnRelease(Card** cards) {
   for (int i = 0; i < ROWS * COLUMNS; i++) {
     Vector2 m = GetMousePosition();
-    if (checkHit(m, cards[i].shape) && IsMouseButtonReleased(MOUSE_LEFT_BUTTON)) {
-      cards[i].opened = true;
+    if (checkHit(m, cards[i]->shape) && IsMouseButtonReleased(MOUSE_LEFT_BUTTON)) {
+      cards[i]->opened = true;
     }
   }
 }
@@ -98,16 +165,18 @@ int main() {
   int w_height = 1000;
   InitWindow(w_width, w_height, "Card Memory Game");
   Rectangle content = { .x =  w_width/2. - 800./2., .y = w_height/2. - 350., .width = 800, .height = 800 };
-  Card *cards = createCards(ROWS, COLUMNS, 187, 187, 10., (Vector2) { .x = 10. + content.x, .y = 10. + content.y });
+  Card **cards = createCards(187, 187, 10., (Vector2) { .x = 10. + content.x, .y = 10. + content.y });
+  CardPair **pairs = createPairs();
+  resetPairs(cards, pairs);
   BeginDrawing();
   ClearBackground(BLACK);
   DrawRectangleRounded(content, .03, 1, DARKGRAY);
   for (int i = 0; i < ROWS * COLUMNS; i++)
-    drawCard(cards[i], WHITE, WHITE);
+    drawCard(*cards[i], WHITE, WHITE);
   while (true) {
-    cardOnHover(cards);
+    cardOnHover(cards, pairs);
     cardOnRelease(cards);
-    updateCards(cards);
+    updateCards(cards, pairs);
     DrawText("PLAY", 430, 50, 50, TEXTCOLOR);
     DrawText("80s", 170, 50, 50, TEXTCOLOR);
     DrawText("0", 700, 50, 50, TEXTCOLOR);
